b3206919d983d9fd715e8a10d54260916bf0b759
calise cheung <cacheung@adobe.com>
2021-04-27 02:56:34 -0700

    swift package test
    swift package test

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
         .library(name: "AEPSignal", targets: ["AEPSignal"])
     ],
     dependencies: [
-       
+      .package(url: "https://github.com/adobe/aepsdk-rulesengine-ios.git", from: "1.0.0"),    
     ],
     targets: [
-         .target(name: "AEPCore",
-                dependencies: ["AEPServices"],
+        .target(name: "AEPCore",
+                dependencies: ["AEPServices", "AEPRulesEngine"],
                 path: "AEPCore/Sources"),
         .target(name: "AEPIdentity",
                 dependencies: ["AEPCore"],
